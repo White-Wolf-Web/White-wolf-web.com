d022767fbb8270d54e6a34eadb42c094cc6646eb
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-10-16 00:00:43 +0200

    Update not-found.js

--- components/not-found.js
+++ components/not-found.js
@@ -2,7 +2,7 @@ const NotFound = () => {
     return (
       <>
         <h2>Page 404</h2>
-        <p>Désolé, le produit demandé est introuvable.</p>
+        <p>Désolé, le produit demandé est introuvable. mais </p>
       </>
     );
   };
