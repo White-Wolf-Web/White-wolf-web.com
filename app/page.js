e6137369b1352682c015512e881f0516bec53e95
Stephane Gamot <papa@iMac-de-Stephane.local>
2023-11-04 12:01:38 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ import { LangageCards } from "@/components/Cards/cardLangage";
 import SeeOtherButton from "@/utils/seeOtherButton";
 import { ContainerBigCardHomePage } from "@/components/Cards/ContainerBigCardHomePage";
 import ContainerBlog from "@/components/Blog/ContainerBlog";
-import MetadataHomePage from "@/components/Metadata/MetadataHomePage";
-//import { Metadata } from "next";
+//import MetadataHomePage from "@/components/Metadata/MetadataHomePage";
+import { Metadata } from "next";
 
 export const metadata = {
 	title: "Agence experte en création de sites web 💻 - SEO",
