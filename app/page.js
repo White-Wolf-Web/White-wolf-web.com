8bc5eff62d994915edfa0af771d93fc17e213333
Stephane Gamot <papa@iMac-de-Stephane.local>
2023-11-05 17:41:39 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -28,21 +28,21 @@ export default function Home() {
 				</h1>
 				<p>
 					Bienvenue chez White Wolf Web, l&apos;<strong>agence web</strong> innovante qui vous accompagne dans la {" "}
-					<Link href="/articles/creation-site-web" className={styles.internalLink}>
+					<span><Link href="/articles/creation-site-web" className={styles.internalLink}>
 						création de site web
-					</Link>
+					</Link></span>
 					{" "}accessible, de {" "}
-					<Link href="/articles/creation-site-web/#site-Ecommerce" className={styles.internalLink}>
+					<span><Link href="/articles/creation-site-web/#site-Ecommerce" className={styles.internalLink}>
 						 boutique en ligne
-					</Link>{" "}
+					</Link></span>{" "}
 					personnalisé, d&apos;un blog personnel, un{" "}
-					<Link href="/articles/web-design" className={styles.internalLink}>
+					<span><Link href="/articles/web-design" className={styles.internalLink}>
 						design responsive
-					</Link>{" "}
+					</Link></span>{" "}
 					, un{" "}
-					<Link href="/articles/referencement-seo-google" className={styles.internalLink}>
+					<span><Link href="/articles/referencement-seo-google" className={styles.internalLink}>
 						référencement naturel - SEO
-					</Link>{" "}
+					</Link></span>{" "}
 					(optimisation des moteurs de recherche tel que Google) ainsi que le marketing digital afin de propulser votre succès en ligne ! 🚀💻{" "}
 				</p>
 				<p>
@@ -56,25 +56,25 @@ export default function Home() {
 				<p>
 					Notre agence Web apporte également de l&apos; aide pour l&apos;hébergement de vos pages web, la configuration d&apos;un nom de domaine personnalisé, une utilisation
 					intelligente des mots-clés, une visibilité maximale de la marque sur les {" "}
-					<Link href="/articles/reseaux-sociaux" className={styles.internalLink}>
+					<span><Link href="/articles/reseaux-sociaux" className={styles.internalLink}>
 						réseaux sociaux
-					</Link>{" "}
+					</Link></span>{" "}
 					(
-					<Link href="/articles/reseaux-sociaux/#facebook" className={styles.colorFacebook}>
+						<span><Link href="/articles/reseaux-sociaux/#facebook" className={styles.colorFacebook}>
 						Facebook
-					</Link>
+					</Link></span>
 					,{" "}
-					<Link href="/articles/reseaux-sociaux/#instagram" className={styles.colorInstagram}>
+					<span><Link href="/articles/reseaux-sociaux/#instagram" className={styles.colorInstagram}>
 						Instagram
-					</Link>
+					</Link></span>
 					,{" "}
-					<Link href="/articles/reseaux-sociaux/#youtube" className={styles.colorYoutube}>
+					<span><Link href="/articles/reseaux-sociaux/#youtube" className={styles.colorYoutube}>
 						Youtube
-					</Link>
+					</Link></span>
 					,
-					<Link href="/articles/reseaux-sociaux/#twitter" className={styles.colorTwitter}>
+					<span><Link href="/articles/reseaux-sociaux/#twitter" className={styles.colorTwitter}>
 						Twitter
-					</Link>
+					</Link></span>
 					...) et les moteurs de recherche. Tous ces éléments vont renforcer la présence de la marque sur le web.
 				</p>
 			</div>
@@ -149,8 +149,8 @@ export default function Home() {
 			<section className={styles.sectionHomePage}>
 				<h2 className={styles.homeH2MarginTop}>Les coûts de création d&apos;un site internet</h2>
 				<p>
-					À l&apos;ère du numérique, disposer d&apos;un <strong>site web</strong> est essentiel pour toute entreprise ou tout particulier désireux d&apos;établir une <Link  href="/articles/referencement-seo-google" className={styles.internalLink}>forte
-					présence en ligne</Link>. Toutefois, les coûts associés à la <strong>création d&apos;un site internet</strong> peuvent souvent sembler insurmontables et déroutants, ce qui fait
+					À l&apos;ère du numérique, disposer d&apos;un <strong>site web</strong> est essentiel pour toute entreprise ou tout particulier désireux d&apos;établir une <span><Link  href="/articles/referencement-seo-google" className={styles.internalLink}>forte
+					présence en ligne</Link></span>. Toutefois, les coûts associés à la <strong>création d&apos;un site internet</strong> peuvent souvent sembler insurmontables et déroutants, ce qui fait
 					que de nombreuses personnes ne savent pas par où commencer. C&apos;est pourquoi ce guide complet a été créé, afin de démystifier les coûts de
 					<strong>création d&apos;un site web</strong>. De l&apos;enregistrement du domaine et des frais d&apos;hébergement aux coûts de <strong>conception</strong> et de <strong>développement</strong>,
 					 les dépenses liées à la création d&apos;un site internet et donnons des indications précieuses sur la manière de maîtriser les coûts. Que
@@ -166,9 +166,9 @@ export default function Home() {
 			<section className={styles.conclusionHomePage}>
 				<h2 className={styles.homeH2}>Notre mission est de fournir un site web d&apos;excellence</h2>
 				<p>
-					La <Link  href="/articles/creation-site-web" className={styles.internalLink}>création de site internet</Link> et la gestion d&apos;une boutique en ligne peut être un défi, en particulier 
+					La <span><Link  href="/articles/creation-site-web" className={styles.internalLink}>création de site internet</Link></span> et la gestion d&apos;une boutique en ligne peut être un défi, en particulier 
 					les auto-entrepreneurs ou une petites entreprises. C&apos;est pourquoi l&apos;agence de création web met tout en œuvre afin d&apos;aider à surmonter ces obstacles. <br></br>
-					L&apos;équipe de white wolf web est composée de dévouée <strong>développeurs web</strong>, de <strong>webdesigners</strong>, de <Link  href="/blog" className={styles.internalLink}>spécialistes du blog</Link>, consultant SEO et chacun est là en vue de vous guider et soutenir à chaque
+					L&apos;équipe de white wolf web est composée de dévouée <strong>développeurs web</strong>, de <strong>webdesigners</strong>, de <span><Link  href="/blog" className={styles.internalLink}>spécialistes du blog</Link></span>, consultant SEO et chacun est là en vue de vous guider et soutenir à chaque
 					étape. Wild Wolf Web est en mesure de fournir des solutions qui non seulement répondent à vos besoins, mais aussi surpassent vos attentes.
 				</p>
 
