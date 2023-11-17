a7c5f8af7b41caa74a4baeda8a99312b6a9eda48
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-11-17 14:13:53 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,24 +15,21 @@ export const metadata = {
 		"Agence Web innovante: création de site web, internet, design, référencement naturel SEO et marketing digital afin de propulser votre succès en ligne!🚀💻",
 	alternates: {
 		canonical: "https://www.white-wolf-web.com/",
-	}
+	},
 };
 export default function Home() {
 	return (
 		<>
 			<div className={styles.bienvenueContent}>
-				<h1>Expert en création de site internet - web & SEO 
-					<br />
-					White Wolf Web
-				</h1>
+				<h1>Expert en création de site internet - web & SEO</h1>
 				<p>
-					Bienvenue chez White Wolf Web, l&apos;<strong>agence web</strong> innovante qui vous accompagne dans la {" "}
+					Bienvenue chez White Wolf Web, l&apos;<strong>agence web</strong> innovante qui vous accompagne dans la{" "}
 					<Link href="/articles/creation-site-web" className={styles.internalLink}>
 						création de site web
-					</Link>
-					{" "}accessible, de {" "}
+					</Link>{" "}
+					accessible, de{" "}
 					<Link href="/articles/creation-site-web/#site-Ecommerce" className={styles.internalLink}>
-						 boutique en ligne
+						boutique en ligne
 					</Link>{" "}
 					personnalisé, d&apos;un blog personnel, un{" "}
 					<Link href="/articles/web-design" className={styles.internalLink}>
@@ -45,16 +42,17 @@ export default function Home() {
 					(optimisation des moteurs de recherche tel que Google) ainsi que le marketing digital afin de propulser votre succès en ligne ! 🚀💻{" "}
 				</p>
 				<p>
-					Chaque projet internet est unique et mérite une attention particulière. Wild Wolf Web est là pour guider & aider à développer votre entreprise sur le web, en offrant des solutions personnalisées adaptées à vos besoins et à votre budget.
+					Chaque projet internet est unique et mérite une attention particulière. Wild Wolf Web est là pour guider & aider à développer votre entreprise sur le
+					web, en offrant des solutions personnalisées adaptées à vos besoins et à votre budget.
 				</p>
 				<p>
-					La refonte d&apos;un <strong>site web professionnel</strong> n&apos;est pas seulement une question de changement de web-design, elle contribue également
-					d&apos;optimiser l&apos;utilisation des mots-clés & des <strong>techniques SEO</strong> qui permette d&apos;améliorer le <strong>référencement naturel</strong>, en particulier si vous prévoyez de
-					créer une <strong>boutique en ligne</strong>.
+					La refonte d&apos;un <strong>site web professionnel</strong> n&apos;est pas seulement une question de changement de web-design, elle contribue
+					également d&apos;optimiser l&apos;utilisation des mots-clés & des <strong>techniques SEO</strong> qui permette d&apos;améliorer le{" "}
+					<strong>référencement naturel</strong>, en particulier si vous prévoyez de créer une <strong>boutique en ligne</strong>.
 				</p>
 				<p>
-					Notre agence Web apporte également de l&apos; aide pour l&apos;hébergement de vos pages web, la configuration d&apos;un nom de domaine personnalisé, une utilisation
-					intelligente des mots-clés, une visibilité maximale de la marque sur les {" "}
+					Notre agence Web apporte également de l&apos; aide pour l&apos;hébergement de vos pages web, la configuration d&apos;un nom de domaine personnalisé,
+					une utilisation intelligente des mots-clés, une visibilité maximale de la marque sur les{" "}
 					<Link href="/articles/reseaux-sociaux" className={styles.internalLink}>
 						réseaux sociaux
 					</Link>{" "}
@@ -77,64 +75,68 @@ export default function Home() {
 					...) et les moteurs de recherche. Tous ces éléments vont renforcer la présence de la marque sur le web.
 				</p>
 			</div>
-			
+
 			<section className={styles.sectionHomePage}>
 				<h2 className={styles.homeH2MarginTop}>La création de site internet professionnel</h2>
 				<p>
 					<strong>Notre agence web</strong> est situé en <strong>Belgique</strong>, au sud de <strong>Bruxelles</strong>. Le but est de vous guider dans la
-					jungle du <strong>web</strong>-design et l&apos;univers du web développeur, afin de transformer votre vision d’un <strong>site internet professionnel</strong>, qui sera livré clé en
-					main, sa stucture & sa sémantique seront parfaitement adapté pour le référencement naturel - SEO et répondra à tous vos besoins. Du <strong>développement web</strong> à la gestion de contenu, en passant au choix de l&apos;hebergeur ou du nom de domaine
-					approprié nous sommes le <strong>webmaster</strong> & developpeur d&apos;application Web qui est présent à chaque étape du projet. <br></br>
+					jungle du <strong>web</strong>-design et l&apos;univers du web développeur, afin de transformer votre vision d’un{" "}
+					<strong>site internet professionnel</strong>, qui sera livré clé en main, sa stucture & sa sémantique seront parfaitement adapté pour le référencement
+					naturel - SEO et répondra à tous vos besoins. Du <strong>développement web</strong> à la gestion de contenu, en passant au choix de l&apos;hebergeur
+					ou du nom de domaine approprié nous sommes le <strong>webmaster</strong> & developpeur d&apos;application Web qui est présent à chaque étape du
+					projet. <br></br>
 					<br></br>Nos propositions :
 				</p>
 
 				<ul>
 					<li className={styles.newLi}>
 						<Link href="/articles/creation-site-web#site-vitrine" className={styles.linkHomePage}>
-						<strong>		Site Vitrine</strong>
+							<strong> Site Vitrine</strong>
 						</Link>
 					</li>
 					<li className={styles.newLi}>
 						<Link href="/articles/creation-site-web#site-Ecommerce" className={styles.linkHomePage}>
-						<strong>Site E-commerce</strong>
+							<strong>Site E-commerce</strong>
 						</Link>
 					</li>
 					<li className={styles.newLi}>Blog</li>
 					<li className={styles.newLi}>
 						<Link href="/articles/creation-site-web#siteInformations" className={styles.linkHomePage}>
-						<strong>Site institutionnel </strong>(info)
+							<strong>Site institutionnel </strong>(info)
 						</Link>
 					</li>
 					<li className={styles.newLi}>
 						<Link href="/articles/creation-site-web#siteMobile" className={styles.linkHomePage}>
-						<strong>Site mobile-first</strong>
+							<strong>Site mobile-first</strong>
 						</Link>
 					</li>
 					<li className={styles.newLi}>Site sur mesure</li>
 					<li className={styles.newLi}>
 						<Link href="/articles/creation-site-web#portfolio" className={styles.linkHomePage}>
-						<strong>Portfolio</strong>
+							<strong>Portfolio</strong>
 						</Link>
 					</li>
 					<li className={styles.newLi}>
 						<Link href="/articles/creation-site-web#landingPage" className={styles.linkHomePage}>
-						<strong>Site d&apos;événements (Landing page</strong>)
+							<strong>Site d&apos;événements (Landing page</strong>)
 						</Link>
 					</li>
-					<li className={styles.newLi}><strong>Application web</strong></li>
+					<li className={styles.newLi}>
+						<strong>Application web</strong>
+					</li>
 					<li className={styles.newLi}>
 						<Link href="/articles/creation-site-web#wordpress" className={styles.linkHomePage}>
-						<strong>Site Wordpress</strong>
+							<strong>Site Wordpress</strong>
 						</Link>
 					</li>
 					<li className={styles.newLi}>
 						<Link href="/articles/creation-site-web#joomla" className={styles.linkHomePage}>
-						<strong>Site Joomla</strong>
+							<strong>Site Joomla</strong>
 						</Link>
 					</li>
 					<li className={styles.newLi}>
 						<Link href="/articles/referencement-seo-google" className={styles.linkHomePage}>
-						<strong>SEO - Référencement naturel</strong>
+							<strong>SEO - Référencement naturel</strong>
 						</Link>
 					</li>
 					<li>&emsp;&ensp;...</li>
@@ -148,13 +150,19 @@ export default function Home() {
 			<section className={styles.sectionHomePage}>
 				<h2 className={styles.homeH2MarginTop}>Les coûts de création d&apos;un site internet</h2>
 				<p>
-					À l&apos;ère du numérique, disposer d&apos;un <strong>site web</strong> est essentiel pour toute entreprise ou tout particulier désireux d&apos;établir une <Link  href="/articles/referencement-seo-google" className={styles.internalLink}>forte
-					présence en ligne</Link>. Toutefois, les coûts associés à la <strong>création d&apos;un site internet</strong> peuvent souvent sembler insurmontables et déroutants, ce qui fait
-					que de nombreuses personnes ne savent pas par où commencer. C&apos;est pourquoi ce guide complet a été créé, afin de démystifier les coûts de
-					<strong>création d&apos;un site web</strong>. De l&apos;enregistrement du domaine et des frais d&apos;hébergement aux coûts de <strong>conception</strong> et de <strong>développement</strong>,
-					 les dépenses liées à la création d&apos;un site internet et donnons des indications précieuses sur la manière de maîtriser les coûts. Que
-					l&apos;on soit propriétaire d&apos;une petite entreprise ou un blogueur désireux de créer son propre <strong>site web</strong>, ce guide apportera les connaissances
-					et les outils nécessaires afin de créer un <strong>site web professionnel</strong> et efficace sans se ruiner. Alors, commençons et dévoilons les secrets des coûts de <strong>création d&apos;un site web</strong> !
+					À l&apos;ère du numérique, disposer d&apos;un <strong>site web</strong> est essentiel pour toute entreprise ou tout particulier désireux
+					d&apos;établir une{" "}
+					<Link href="/articles/referencement-seo-google" className={styles.internalLink}>
+						forte présence en ligne
+					</Link>
+					. Toutefois, les coûts associés à la <strong>création d&apos;un site internet</strong> peuvent souvent sembler insurmontables et déroutants, ce qui
+					fait que de nombreuses personnes ne savent pas par où commencer. C&apos;est pourquoi ce guide complet a été créé, afin de démystifier les coûts de
+					<strong>création d&apos;un site web</strong>. De l&apos;enregistrement du domaine et des frais d&apos;hébergement aux coûts de{" "}
+					<strong>conception</strong> et de <strong>développement</strong>, les dépenses liées à la création d&apos;un site internet et donnons des indications
+					précieuses sur la manière de maîtriser les coûts. Que l&apos;on soit propriétaire d&apos;une petite entreprise ou un blogueur désireux de créer son
+					propre <strong>site web</strong>, ce guide apportera les connaissances et les outils nécessaires afin de créer un{" "}
+					<strong>site web professionnel</strong> et efficace sans se ruiner. Alors, commençons et dévoilons les secrets des coûts de{" "}
+					<strong>création d&apos;un site web</strong> !
 				</p>
 				<SeeOtherButton href="/admin/price" text="En savoir +++" />
 			</section>
@@ -165,16 +173,25 @@ export default function Home() {
 			<section className={styles.conclusionHomePage}>
 				<h2 className={styles.homeH2}>Notre mission est de fournir un site web d&apos;excellence</h2>
 				<p>
-					La <Link  href="/articles/creation-site-web" className={styles.internalLink}>création de site internet</Link> et la gestion d&apos;une boutique en ligne peut être un défi, en particulier 
-					les auto-entrepreneurs ou une petites entreprises. C&apos;est pourquoi l&apos;agence de création web met tout en œuvre afin d&apos;aider à surmonter ces obstacles. <br></br>
-					L&apos;équipe de white wolf web est composée de dévouée <strong>développeurs web</strong>, de <strong>webdesigners</strong>, de <Link  href="/blog" className={styles.internalLink}>spécialistes du blog</Link>, consultant SEO et chacun est là en vue de vous guider et soutenir à chaque
-					étape. Wild Wolf Web est en mesure de fournir des solutions qui non seulement répondent à vos besoins, mais aussi surpassent vos attentes.
+					La{" "}
+					<Link href="/articles/creation-site-web" className={styles.internalLink}>
+						création de site internet
+					</Link>{" "}
+					et la gestion d&apos;une boutique en ligne peut être un défi, en particulier les auto-entrepreneurs ou une petites entreprises. C&apos;est pourquoi
+					l&apos;agence de création web met tout en œuvre afin d&apos;aider à surmonter ces obstacles. <br></br>
+					L&apos;équipe de white wolf web est composée de dévouée <strong>développeurs web</strong>, de <strong>webdesigners</strong>, de{" "}
+					<Link href="/blog" className={styles.internalLink}>
+						spécialistes du blog
+					</Link>
+					, consultant SEO et chacun est là en vue de vous guider et soutenir à chaque étape. Wild Wolf Web est en mesure de fournir des solutions qui non
+					seulement répondent à vos besoins, mais aussi surpassent vos attentes.
 				</p>
 
 				<p>
-					Si vous êtes à <strong>Bruxelles</strong>, en <strong>Belgique</strong>, ou n&apos;importe où dans le monde, l&apos;agence web serait ravis de vous aider à transformer une vision en réalité. Ne
-					laissez pas la complexité du web vous décourager - avec <strong>White Wolf Web</strong> à vos côtés, c&apos;est avoir un partenaire de confiance qui permet de naviguer dans la jungle
-					du web. Ensemble, nous pouvons réaliser de grandes choses. Notre agence web est impatients de travailler avec votre entreprise !
+					Si vous êtes à <strong>Bruxelles</strong>, en <strong>Belgique</strong>, ou n&apos;importe où dans le monde, l&apos;agence web serait ravis de vous
+					aider à transformer une vision en réalité. Ne laissez pas la complexité du web vous décourager - avec <strong>White Wolf Web</strong> à vos côtés,
+					c&apos;est avoir un partenaire de confiance qui permet de naviguer dans la jungle du web. Ensemble, nous pouvons réaliser de grandes choses. Notre
+					agence web est impatients de travailler avec votre entreprise !
 				</p>
 			</section>
 			<ContainerBlog />
