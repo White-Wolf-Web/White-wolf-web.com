26632f318a5558ca0a2842b1faa17c8e3b26f137
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-12-06 16:19:57 +0100

    Update MetadataCreationWebPage.js

--- components/Metadata/MetadataCreationWebPage.js
+++ components/Metadata/MetadataCreationWebPage.js
@@ -3,11 +3,11 @@ const MetadataCreationWebPage = {
   "@type": "Article",
   "name": "Guide complet sur la création de site web professionnel",
    "headline": "Les Étapes Essentielles pour Créer un Site Web Professionnel",
+   "url": "https://www.white-wolf-web.com/articles/creation-site-web",
    "author": {
     "@type": "Person",
     "name": "Stéphane G"
   },
-  "url": "https://www.white-wolf-web.com/articles/creation-site-web",
   "description": "Un article détaillé offrant des conseils et des stratégies pour la création de sites web professionnels, couvrant des sujets tels que la compréhension du public ciblé, le choix d'un nom de domaine, l'utilisation de plateformes CMS, la création de contenu, le SEO, et la maintenance.",
   "publisher": {
     "@type": "Organization",
@@ -19,7 +19,7 @@ const MetadataCreationWebPage = {
 		"https://www.white-wolf-web.com/Assets/Images/Webp/creation-site/creation-site-web-2-XXL.webp",
 		"https://www.white-wolf-web.com/Assets/Images/Webp/creation-site/creation-site-web-3-XXL.webp"
 	],
-  "datePublished": "2023-07-01",
+  "datePublished": "2023-07-01T10:00:00+02:00",
   "mainEntityOfPage": {
     "@type": "WebPage",
     "@id": "https://www.white-wolf-web.com/articles/creation-site-web"
