2dbecc432ceb5265605718c749fe446e51b65084
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-12-06 16:13:27 +0100

    Update MetadataCreationWebPage.js

--- components/Metadata/MetadataCreationWebPage.js
+++ components/Metadata/MetadataCreationWebPage.js
@@ -2,6 +2,11 @@ const MetadataCreationWebPage = {
   "@context": "http://schema.org",
   "@type": "Article",
   "name": "Guide complet sur la création de site web professionnel",
+   "headline": "Les Étapes Essentielles pour Créer un Site Web Professionnel",
+   "author": {
+    "@type": "Person",
+    "name": "Stéphane G"
+  },
   "url": "https://www.white-wolf-web.com/articles/creation-site-web",
   "description": "Un article détaillé offrant des conseils et des stratégies pour la création de sites web professionnels, couvrant des sujets tels que la compréhension du public ciblé, le choix d'un nom de domaine, l'utilisation de plateformes CMS, la création de contenu, le SEO, et la maintenance.",
   "publisher": {
@@ -9,16 +14,16 @@ const MetadataCreationWebPage = {
     "name": "White Wolf Web",
     "url": "https://www.white-wolf-web.com"
   },
-  "image": [
-    "https://www.white-wolf-web.com/Assets/Images/Webp/creation-site/creation-site-web-1-XL.webp",
-    "https://www.white-wolf-web.com/Assets/Images/Webp/creation-site/creation-site-web-2-XXL.webp",
-    "https://www.white-wolf-web.com/Assets/Images/Webp/creation-site/creation-site-web-3-XXL.webp"
-  ],
-  "datePublished": "1er juillet 2023",
+	"image": [
+		"https://www.white-wolf-web.com/Assets/Images/Webp/creation-site/creation-site-web-1-XL.webp",
+		"https://www.white-wolf-web.com/Assets/Images/Webp/creation-site/creation-site-web-2-XXL.webp",
+		"https://www.white-wolf-web.com/Assets/Images/Webp/creation-site/creation-site-web-3-XXL.webp"
+	],
+  "datePublished": "2023-07-01",
   "mainEntityOfPage": {
     "@type": "WebPage",
     "@id": "https://www.white-wolf-web.com/articles/creation-site-web"
   },
   "articleSection": "Création de sites web"
-}
+};
 export default MetadataCreationWebPage;
